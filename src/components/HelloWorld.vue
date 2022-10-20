bfe4b90710ebd219cff76038597107d40ba1b613
Vít Stanislav <slaweet@gmail.com>
2022-10-20 10:33:10 +0200

    ✨ Create Montessori cards view

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,63 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="container">
+    <div v-for="(src, name) in photos" :key="name" class="card">
+      <img :src="src" :alt="name" width="210"/>
+      <div class="label">{{ name.replace('./', '').replace('.jpg', '') }}</div>
+    </div>
   </div>
 </template>
 
 <script>
+  function importAll(r) {
+    return r.keys().reduce((accumulator, key) => {
+      accumulator[key] = r(key);
+      return accumulator;
+    }, {});
+  }
+  const photos = importAll(require.context('../assets/photos/', true, /\.jpg/));
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
-  }
+  },
+  data: function() {
+return {
+    photos,
+  };
+}
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.container {
+  max-width: 800px;
+  margin-top: 2rem;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
+
+.card {
   display: inline-block;
-  margin: 0 10px;
+  border: 1px solid #aaa;
+  padding: 0.25rem;
+
 }
-a {
-  color: #42b983;
+
+.card:nth-child(n+4) {
+  border-top: 0;
+}
+
+.card:nth-child(3n+2),
+.card:nth-child(3n+3) {
+  border-left: 0;
+}
+
+.card img {
+  border-radius: 0.5rem 0.5rem 0 0;
+}
+
+.label {
+  padding: 0.25rem;
+  font-size: 1.65rem;
 }
 </style>
